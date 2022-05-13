ac1b487083edc23330353ed94e4ef5c82fc62b81
n0xx <smc.8645@gmail.com>
2022-05-12 20:23:45 -0500

    Added book input fields

--- script.js
+++ script.js
@@ -62,6 +62,97 @@ function newCard(book) {
 	child22.textContent = book.showInfo();
 }
 
+function insertInfoCard() {
+	// Information-display div
+	const content = document.querySelector('.content')
+	const container = document.createElement('div');
+	container.id = 'insert-info-card';
+	content.appendChild(container);
+
+	// Header of info-display
+	const header = document.createElement('div')
+	header.id = 'header-info-card';
+	header.textContent = 'Insert book information';
+	container.appendChild(header);
+
+	// Info fields flex container
+	const infoFields = document.createElement('div');
+	infoFields.id = 'info-fields';
+	container.appendChild(infoFields);
+
+	//Title field flex container
+	const infoFieldTitle = document.createElement('div');
+	infoFieldTitle.id = 'infoFieldTitle';
+	infoFields.appendChild(infoFieldTitle);
+
+	// Title field text
+	const titleFieldText = document.createElement('div');
+	titleFieldText.textContent = 'Title:';
+	infoFieldTitle.appendChild(titleFieldText);
+
+	// Title field input
+	const titleFieldInput = document.createElement('input');
+	titleFieldInput.id = 'title-field-input';
+	infoFieldTitle.appendChild(titleFieldInput);
+
+	//Author field flex container
+	const infoFieldAuthor = document.createElement('div');
+	infoFieldAuthor.id = 'infoFieldAuthor';
+	infoFields.appendChild(infoFieldAuthor);
+
+	// Author field text
+	const authorFieldText = document.createElement('div');
+	authorFieldText.textContent = 'Author:';
+	infoFieldAuthor.appendChild(authorFieldText);
+
+	// Author field input
+	const authorFieldInput = document.createElement('input');
+	authorFieldInput.id = 'authorFieldInput';
+	infoFieldAuthor.appendChild(authorFieldInput);
+
+	// Pages field flex container
+	const infoFieldPages = document.createElement('div');
+	infoFieldPages.id = 'infoFieldPages';
+	infoFields.appendChild(infoFieldPages);
+
+	// Pages field text
+	const pagesFieldText = document.createElement('div');
+	pagesFieldText.textContent = 'Pages:';
+	infoFieldPages.appendChild(pagesFieldText);
+
+	// Pages field input
+	const pagesFieldInput = document.createElement('input');
+	pagesFieldInput.id = 'pagesFieldInput';
+	infoFieldPages.appendChild(pagesFieldInput);
+
+	// Read field flex container
+	const infoFieldRead = document.createElement('div');
+	infoFieldRead.id = 'infoFieldRead';
+	infoFields.appendChild(infoFieldRead);
+
+	// Read field text
+	const readFieldText = document.createElement('div');
+	readFieldText.textContent = 'Read:';
+	infoFieldRead.appendChild(readFieldText);
+
+	// Read field input
+	const readFieldInput = document.createElement('input');
+	readFieldInput.id = 'readFieldInput';
+	infoFieldRead.appendChild(readFieldInput);
+
+	// Insert book button
+	const insertBook = document.createElement('button');
+	insertBook.id = 'insert-book-btn';
+	insertBook.textContent = 'Insert book';
+	infoFields.appendChild(insertBook);
+}
+
+// Display insert-info cards
+const btn = document.getElementById('add-book-btn');
+btn.addEventListener('click', function() {
+	insertInfoCard();
+});
+
 function displayBooks() {
 	myLibrary.forEach(item => {
 		newCard(item);
